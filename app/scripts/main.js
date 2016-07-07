f32773bfe810d09b3f554ca72d1721914a8a7ec0
srkela <djenka76@gmail.com>
2016-07-07 12:13:11 +0200

    Content Updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,40 +4,40 @@ $(document).ready(function(){
   'use strict';
 
   //NAVBAR ANIMATION
-  var cbpAnimatedHeader = (function() {
+  (function() {
 
-  var docElem = document.documentElement,
-    header = document.querySelector( '.navbar-default' ),
-    dropdown = document.querySelector('.dropdown-menu'),
-    didScroll = false,
-    changeHeaderOn = 300;
+    var docElem = document.documentElement,
+      header = document.querySelector( '.navbar-default' ),
+      dropdown = document.querySelector('.dropdown-menu'),
+      didScroll = false,
+      changeHeaderOn = 300;
 
-  function init() {
-    window.addEventListener( 'scroll', function( event ) {
-      if( !didScroll ) {
-        didScroll = true;
-        setTimeout( scrollPage, 250 );
+    function init() {
+      window.addEventListener( 'scroll', function() {
+        if( !didScroll ) {
+          didScroll = true;
+          setTimeout( scrollPage, 250 );
+        }
+      }, false );
+    }
+
+    function scrollPage() {
+      var sy = scrollY();
+      if ( sy >= changeHeaderOn ) {
+        classie.add( header, 'navbar-shrink' );
       }
-    }, false );
-  }
-
-  function scrollPage() {
-    var sy = scrollY();
-    if ( sy >= changeHeaderOn ) {
-      classie.add( header, 'navbar-shrink' );
+      else {
+        classie.remove( header, 'navbar-shrink' );
+      }
+      if ( sy >= changeHeaderOn ) {
+        classie.add( dropdown, 'dropdown-shrink' );
+      }
+      else {
+        classie.remove( dropdown, 'dropdown-shrink' );
+      }
+      didScroll = false;
     }
-    else {
-      classie.remove( header, 'navbar-shrink' );
-    }
-    if ( sy >= changeHeaderOn ) {
-      classie.add( dropdown, 'dropdown-shrink' );
-    }
-    else {
-      classie.remove( dropdown, 'dropdown-shrink' );
-    }
-    didScroll = false;
-  }
-
+    //console.log(classie);
     function scrollY() {
       return window.pageYOffset || docElem.scrollTop;
     }
